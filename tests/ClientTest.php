f357fc592430d95efc6308a6569b083269195ba0
cemilakkoc <=>
2022-02-18 00:55:34 +0300

    added tests for post requests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -7,10 +7,28 @@ beforeEach(function () {
     Route::get('get-route', function () {
         return 'get result';
     });
+
+    Route::post('post-route', function () {
+        return request()->all();
+    });
 });
 
 it('can perform a get request', function () {
     $content = Client::make()->get('/get-route')->content();
 
     expect($content)->toBe('get result');
+});
+
+it('can perform a post request', function () {
+    $statusCode = Client::make()->post('/post-route')->getStatusCode();
+    expect($statusCode)->toBe(200);
+});
+
+it('can perform a post request with data', function () {
+    $content = Client::make()->post('/post-route', $data = [
+        "username" => "cemil",
+        "email" => "cemil@akko.cc"
+    ])->getOriginalContent();
+    
+    expect($content)->toBe($data);
 });
